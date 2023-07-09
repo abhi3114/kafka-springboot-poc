f1688565b7fba3971feedb95a931d370a741f67c
abhi3114 <chavanabhi3114@gmail.com>
2023-07-09 13:44:41 +0530

    kafka poc
    kafka poc

---
+++ consumer/src/main/java/com/kafkaconsumer/consumer/entity/LibraryEventType.java
@@ -0,0 +1,6 @@
+package com.kafkaconsumer.consumer.entity;
+
+public enum LibraryEventType {
+    NEW,
+    UPDATE
+}
---
+++ producer/src/main/java/com/kafkaproducer/producer/domain/LibraryEventType.java
@@ -0,0 +1,6 @@
+package com.kafkaproducer.producer.domain;
+
+public enum LibraryEventType {
+    NEW,
+    UPDATE
+}
